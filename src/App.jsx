59a6cc6978eccd12ad98a7043540ac5b68669b51
DANTE VERDI GUTIERREZ <47573312@ort>
2024-05-13 14:33:28 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Hola from './components/Hola.jsx'
+
+function App() {
+  return (
+    <>
+    
+    <Hola></Hola>
+    {/* <Sobremi></Sobremi>
+    <Conocimientos></Conocimientos>
+    <Experiencia></Experiencia>
+    <Recomendaciones></Recomendaciones>
+    <Educacion></Educacion>
+    <Ceretificados></Ceretificados>
+    <Contacto></Contacto> */}
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hola.jsx
@@ -0,0 +1,12 @@
+
+
+const Hola = () => {
+    return (
+        <div class="center">
+            <h1>Hola!</h1>
+            <p>Soy dante, ingeniero en sistemas.</p>
+        </div>
+    )
+}
+
+export default Hola
