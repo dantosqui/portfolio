37182773b27f36565485be566a3b7083ef62ff6d
dantosqui <116527161+dantosqui@users.noreply.github.com>
2024-05-23 01:02:08 +0000

    ayy tan poquito que me falta del css y me tengo que ir a dormir que bronca grrr

--- src/components/experiencia/Experiencia.jsx
+++ src/components/experiencia/Experiencia.jsx
@@ -7,7 +7,7 @@ const Experiencia = () => {
         <>
         <div id="experiencia">
         <h3 class="center"> Experiencia</h3>
-        <HorizontalCard  title="UI designer" subtitle="Veil. (2026-2030)" text="Pase cuatro años como diseñador de UI en esta empresa y gane mucha experiencia de trabajo para el UI y el diseño de interfaces de usuario. Estos años los pase trabajando en la empresa y en esta empresa los años que los pase trabajando los pase ganando experiencia de trabajo para posteriores trabajos en el area." icon="https://png.pngtree.com/png-vector/20191126/ourmid/pngtree-web-design-icon-png-image_2038483.jpg"></HorizontalCard>
+        <HorizontalCard  title="UI designer" subtitle="Valve. (2026-2030)" text="Pase cuatro años como diseñador de UI en esta empresa y gane mucha experiencia de trabajo para el UI y el diseño de interfaces de usuario. Estos años los pase trabajando en la empresa y en esta empresa los años que los pase trabajando los pase ganando experiencia de trabajo para posteriores trabajos en el area." icon="https://png.pngtree.com/png-vector/20191126/ourmid/pngtree-web-design-icon-png-image_2038483.jpg"></HorizontalCard>
         <HorizontalCard  title="Fundador y Developer" subtitle="Hangover. (2025-actualidad)" text="Con mis compañeros de la iniciativa Hangover iniciamos la iniciativa conocida como hangover que consiste en una iniciativa de empresa especifica para lograr los objetivos que nos ponemos como equipo para poder traer un producto satisfactorio que pueda conllevar las responsabilidades de nusetra empresa" icon={hol}></HorizontalCard>
         </div>
         </>
--- src/components/recomendaciones/Recomendaciones.jsx
+++ src/components/recomendaciones/Recomendaciones.jsx
@@ -7,7 +7,11 @@ return(
     <div id="recomendaciones">
     <h3 class="center" style={{marginTop:'20px'}} >Recomendaciones</h3>
     <div class="quotes"> 
-        <Quote text="Es re pro"></Quote>
+        <Quote text="Para quien corresponda:
+Es un placer recomendar a Dante, quien demostró ser un empleado excepcional durante su tiempo trabajando conmigo. Dante se destacó por su dedicación, ética laboral y habilidades sobresalientes para resolver problemas de manera eficiente. Es una persona confiable, responsable y comprometida con su trabajo.
+Sin duda alguna, Dante será un activo valioso para cualquier organización. Lo recomiendo sin reservas.
+Atentamente,
+Zareke"></Quote>
         <Quote text="A quien corresponda:
 Es un placer recomendar a [dante]. He tenido la oportunidad de interactuar con [él/ella] y puedo dar fe de su gran capacidad, dedicación y ética de trabajo excepcional.
 [dante] tiene un profundo conocimiento en [informatica] y siempre se desempeña con profesionalismo y eficiencia. Sin duda, sería un gran activo para cualquier organización.
